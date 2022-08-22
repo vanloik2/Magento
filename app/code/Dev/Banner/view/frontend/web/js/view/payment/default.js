12438630f1de74f043626804eaee32b785d615cc
vanloik2 <nguyenvanloi0105212@gmail.com>
2022-08-22 18:01:49 +0700

    .

---
+++ app/code/Dev/Banner/view/frontend/web/js/view/payment/default.js
@@ -0,0 +1,14 @@
+define(
+    [
+        'Dev_Banner/js/view/payment/default'
+    ],
+    function (Component) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                template: '%path to template%'
+            },
+            // add required logic here
+        });
+    }
+);
